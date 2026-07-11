ed43bf151fa21916951ba41edf24c7cf4d6f61d4
taspiajannatshifa-gif <taspiajannatshifa@gmail.com>
2026-07-11 17:15:08 +0600

    Update and rename Code--13 to Code--13.py

--- Code--13.py
+++ Code--13.py
@@ -1,4 +1,4 @@
-num1 = float(input("Enter first number: "))
+.num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
 average = (num1 + num2) / 2
 print("The average is:", average)
